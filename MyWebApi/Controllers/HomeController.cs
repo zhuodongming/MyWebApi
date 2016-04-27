4a150089b006fdc89e7c1fbc9c10be7a73a5c20d
zhuodongming <605395451@qq.com>
2016-04-27 12:09:17 +0800

    增加NanoProfiler性能监控

---
+++ MyWebApi/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using EF.Diagnostics.Profiling;
+
+namespace MyWebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [OverrideActionFilters]
+        [OverrideAuthentication]
+        [OverrideAuthorization]
+        public async Task<IHttpActionResult> Index()
+        {
+            using (ProfilingSession.Current.Step("HomeController.Index"))
+            {
+                await Task.Delay(100);
+                using (ProfilingSession.Current.Step(() => "Print item: "))
+                {
+                    return Ok("Index");
+                }
+            }
+        }
+    }
+}
