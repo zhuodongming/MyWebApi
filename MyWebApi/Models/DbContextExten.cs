083b8c0c5ea4b0aefcb17e60999d17b63165d10d
zhuodongming <605395451@qq.com>
2016-05-04 11:21:01 +0800

    添加DbContext的扩展方法、添加log4net的logHelper操作方法

---
+++ MyWebApi/Models/DbContextExten.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyWebApi.Models
+{
+    /// <summary>
+    /// DbContext添加扩展方法
+    /// </summary>
+    public static class DbContextExten
+    {
+        /// <summary>
+        /// 执行存储过程
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="commandText"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static async Task<IList<TEntity>> ExecuteStoredProcedureListAsync<TEntity>(this DbContext context, string commandText, params object[] parameters) where TEntity : class
+        {
+            if (parameters != null && parameters.Length > 0)
+            {
+                for (int i = 0; i <= parameters.Length - 1; i++)
+                {
+                    var p = parameters[i] as DbParameter;
+                    if (p == null)
+                        throw new Exception("Not support parameter type");
+
+                    commandText += i == 0 ? " " : ", ";
+
+                    commandText += "@" + p.ParameterName;
+                    if (p.Direction == ParameterDirection.InputOutput || p.Direction == ParameterDirection.Output)
+                    {
+
+                        commandText += " output";
+                    }
+                }
+            }
+
+            var result = await context.Database.SqlQuery<TEntity>(commandText, parameters).ToListAsync();
+
+            bool acd = context.Configuration.AutoDetectChangesEnabled;
+            try
+            {
+                context.Configuration.AutoDetectChangesEnabled = false;
+
+                for (int i = 0; i < result.Count; i++)
+                    result[i] = context.Set<TEntity>().Attach(result[i]);
+            }
+            finally
+            {
+                context.Configuration.AutoDetectChangesEnabled = acd;
+            }
+
+            return result;
+        }
+    }
+}
--- MyWebApi/Models/LogHelper.cs
+++ MyWebApi/Models/LogHelper.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Web;
+using log4net;
 
 namespace MyWebApi.Models
 {
     public class LogHelper
     {
+        private static ILog logger = LogManager.GetLogger(typeof(LogHelper));
+
+        public static void Debug(object message, Exception exception = null)
+        {
+            if (logger.IsDebugEnabled)
+            {
+                logger.Debug(message, exception);
+            }
+        }
+
+        public static void Info(object message, Exception exception = null)
+        {
+            if (logger.IsInfoEnabled)
+            {
+                logger.Info(message, exception);
+            }
+        }
+
+        public static void Warn(object message, Exception exception = null)
+        {
+            if (logger.IsWarnEnabled)
+            {
+                logger.Warn(message, exception);
+            }
+        }
+
+        public static void Error(object message, Exception exception = null)
+        {
+            if (logger.IsErrorEnabled)
+            {
+                logger.Error(message, exception);
+            }
+        }
+
         /// <summary>
         /// 获取执行带参数的sql、存储过程时的完整sql
         /// </summary>
