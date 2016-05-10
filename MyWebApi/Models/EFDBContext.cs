4e75071c3842d40db1a7eac23f38f4700ec6b517
zhuodongming <605395451@qq.com>
2016-05-10 17:07:56 +0800

    添加log4net的程序集配置信息

--- MyWebApi/Models/EFDBContext.cs
+++ MyWebApi/Models/EFDBContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Reflection;
 using System.Web;
@@ -16,6 +17,16 @@ namespace MyWebApi.Models
             //通过反射一次性将表进行映射 FluentAPI
             modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
 
+            //var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
+            //.Where(type => !String.IsNullOrEmpty(type.Namespace))
+            //.Where(type => type.BaseType != null && type.BaseType.IsGenericType &&
+            //    type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+            //foreach (var type in typesToRegister)
+            //{
+            //    dynamic configurationInstance = Activator.CreateInstance(type);
+            //    modelBuilder.Configurations.Add(configurationInstance);
+            //}
+
             //用于在DbContext中配置model的非公有属性映射到数据库表中
             modelBuilder.Types().Configure(d =>
             {
@@ -25,6 +36,9 @@ namespace MyWebApi.Models
                     d.Property(p).HasColumnName(p.Name);
                 }
             });
+
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- MyWebApi/Properties/AssemblyInfo.cs
+++ MyWebApi/Properties/AssemblyInfo.cs
@@ -33,3 +33,6 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用 "*":
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+//设置log4net的配置信息
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
